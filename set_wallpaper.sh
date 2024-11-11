d22e75cacc68903fd72c210000c6c7f42493be2a
Himanshu <himanshushukla71.9@gmail.com>
2024-11-11 15:45:09 +0530

    Added : Pywal Script Added

---
+++ set_wallpaper.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+WALLPAPER_DIRECTORY=~/wallpapers
+
+
+RANDOM_WALLPAPER=$(find $WALLPAPER_DIRECTORY -type f -name "*.jpg" -o -name "*.png" | shuf -n 1)
+
+
+feh --bg-scale $RANDOM_WALLPAPER
+wal -i $RANDOM_WALLPAPER
+xdotool key super+F5
